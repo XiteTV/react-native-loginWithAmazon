221ed5428d70ec44dac3dd28674d56aa095b6fe5
Ruen Offerman <ruen_offerman@hotmail.com>
2019-05-09 12:23:09 +0200

    Added javascript code to make it easy to use

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-
+/* Dependencies
+================================================================== */
 import { NativeModules } from 'react-native';
 
+/* NativeModule
+================================================================== */
 const { RNReactNativeLoginWithAmazon } = NativeModules;
 
-export default RNReactNativeLoginWithAmazon;
+/* AmazonLogin
+================================================================== */
+const AmazonLogin = {
+	authorizationStatus: RNReactNativeLoginWithAmazon.authorizationStatus,
+	userInfoStatus: RNReactNativeLoginWithAmazon.userInfoStatus,
+	requestAuthorization: async ({
+		onSuccess = () => {},
+		onError = () => {},
+		onCancel = () => {},
+	}) => {
+		try {
+			const response = await RNReactNativeLoginWithAmazon.requestAuthorization();
+			const { authorizationStatus } = this;
+			console.log('[Authorization]', JSON.stringify(response, null, 2));
+			onSuccess(response);
+		} catch (e) {
+			console.log('[Authorization] - error', JSON.stringify(e, null, 2));
+			switch (e.status) {
+				case authorizationStatus.CANCEL:
+					onCancel(response);
+					break;
+				case authorizationStatus.ERROR:
+					onError(response);
+					break;
+				default:
+					break;
+			}
+		}
+	},
+	getUserInformation: async ({ onSuccess = () => {}, onError = () => {} }) => {
+		try {
+			const response = await RNReactNativeLoginWithAmazon.getUserInformation();
+			const { userInfoStatus } = this;
+			console.log('[UserInformation]', JSON.stringify(response, null, 2));
+			onSuccess(response);
+		} catch (e) {
+			console.log('[UserInformation] - error', JSON.stringify(e, null, 2));
+			onError(e);
+		}
+	},
+	signOut: () => {
+		// RNReactNativeLoginWithAmazon.signOut();
+	},
+};
+
+/* Export
+================================================================== */
+export default AmazonLogin;
