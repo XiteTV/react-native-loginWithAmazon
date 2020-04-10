528a3354163609c943eaabe46d0ca92811ddbe83
Aakash Sigdel <aakash.sigdel@gmail.com>
2020-04-10 11:32:40 +0200

    add prettier and format code

--- index.js
+++ index.js
@@ -9,63 +9,63 @@ const { RNReactNativeLoginWithAmazon } = NativeModules;
 /* AmazonLogin
 ================================================================== */
 const AmazonLogin = {
-	authorizationStatus: RNReactNativeLoginWithAmazon.authorizationStatus,
-	userInfoStatus: RNReactNativeLoginWithAmazon.userInfoStatus,
-	requestAuthorization: async ({
-		onSuccess = () => { },
-		onError = () => { },
-		onCancel = () => { },
-	}) => {
-		try {
-			const response = await RNReactNativeLoginWithAmazon.requestAuthorization();
-			const { authorizationStatus } = this;
-			console.log('[Authorization]', JSON.stringify(response, null, 2));
-			onSuccess(response);
-			return Promise.resolve(response);
-		} catch (e) {
-			console.log('[Authorization] - error', JSON.stringify(e, null, 2));
-			switch (e.status) {
-				case authorizationStatus.CANCEL:
-					onCancel(response);
-					break;
-				case authorizationStatus.ERROR:
-					onError(response);
-					break;
-				default:
-					break;
-			}
+  authorizationStatus: RNReactNativeLoginWithAmazon.authorizationStatus,
+  userInfoStatus: RNReactNativeLoginWithAmazon.userInfoStatus,
+  requestAuthorization: async ({
+    onSuccess = () => {},
+    onError = () => {},
+    onCancel = () => {},
+  }) => {
+    try {
+      const response = await RNReactNativeLoginWithAmazon.requestAuthorization();
+      const { authorizationStatus } = this;
+      console.log('[Authorization]', JSON.stringify(response, null, 2));
+      onSuccess(response);
+      return Promise.resolve(response);
+    } catch (e) {
+      console.log('[Authorization] - error', JSON.stringify(e, null, 2));
+      switch (e.status) {
+        case authorizationStatus.CANCEL:
+          onCancel(response);
+          break;
+        case authorizationStatus.ERROR:
+          onError(response);
+          break;
+        default:
+          break;
+      }
 
-			return Promise.reject(e);
-		}
-	},
-	getUserInformation: async ({ onSuccess = () => { }, onError = () => { } }) => {
-		try {
-			const response = await RNReactNativeLoginWithAmazon.getUserInformation();
-			const { userInfoStatus } = this;
-			console.log('[UserInformation]', JSON.stringify(response, null, 2));
-			onSuccess(response);
-			return Promise.resolve(response);
-		} catch (e) {
-			console.log('[UserInformation] - error', JSON.stringify(e, null, 2));
-			onError(e);
-			return Promise.reject(e);
-		}
-	},
-	signOut: async ({ onSuccess = () => { }, onError = () => { } }) => {
-		try {
-			const response = await RNReactNativeLoginWithAmazon.signOut();
-			console.log('[Authorization Signout]', JSON.stringify(response, null, 2));
-			onSuccess(response);
-			return Promise.resolve(response);
-		} catch (e) {
-			console.log(
-				'[Authorization Signout] - error',
-				JSON.stringify(e, null, 2)
-			);
-			onError(e);
-			return Promise.reject(e);
-		}
-	},
+      return Promise.reject(e);
+    }
+  },
+  getUserInformation: async ({ onSuccess = () => {}, onError = () => {} }) => {
+    try {
+      const response = await RNReactNativeLoginWithAmazon.getUserInformation();
+      const { userInfoStatus } = this;
+      console.log('[UserInformation]', JSON.stringify(response, null, 2));
+      onSuccess(response);
+      return Promise.resolve(response);
+    } catch (e) {
+      console.log('[UserInformation] - error', JSON.stringify(e, null, 2));
+      onError(e);
+      return Promise.reject(e);
+    }
+  },
+  signOut: async ({ onSuccess = () => {}, onError = () => {} }) => {
+    try {
+      const response = await RNReactNativeLoginWithAmazon.signOut();
+      console.log('[Authorization Signout]', JSON.stringify(response, null, 2));
+      onSuccess(response);
+      return Promise.resolve(response);
+    } catch (e) {
+      console.log(
+        '[Authorization Signout] - error',
+        JSON.stringify(e, null, 2)
+      );
+      onError(e);
+      return Promise.reject(e);
+    }
+  },
 };
 
 /* Export
