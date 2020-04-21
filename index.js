de30c7cef52dfc1697453013474158c887c3d74f
Aakash Sigdel <aakash.sigdel@gmail.com>
2020-04-21 09:58:13 +0200

    remove duplicate decleration

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ export async function requestAuthorization() {
   const authorizationStatus = RNReactNativeLoginWithAmazon.authorizationStatus;
   try {
     const response = await RNReactNativeLoginWithAmazon.requestAuthorization();
-    const { authorizationStatus } = this;
     console.log('[Authorization]', JSON.stringify(response, null, 2));
     // onSuccess(response);
     return Promise.resolve(response);
@@ -37,7 +36,6 @@ export async function getUserInformation() {
   try {
     const userInfoStatus = RNReactNativeLoginWithAmazon.userInfoStatus;
     const response = await RNReactNativeLoginWithAmazon.getUserInformation();
-    const { userInfoStatus } = this;
     console.log('[UserInformation]', JSON.stringify(response, null, 2));
     // onSuccess(response);
     return Promise.resolve(response);
