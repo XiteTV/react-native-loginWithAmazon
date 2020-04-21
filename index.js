bd4091ae94151c0db0102985fd30cac96bb5d711
Aakash Sigdel <aakash.sigdel@gmail.com>
2020-04-21 14:51:49 +0200

    simplify implementation

--- index.js
+++ index.js
@@ -9,39 +9,13 @@ const { RNReactNativeLoginWithAmazon } = NativeModules;
 /* AmazonLogin
 ================================================================== */
 export async function requestAuthorization() {
-  const authorizationStatus = RNReactNativeLoginWithAmazon.authorizationStatus;
-  try {
-    const response = await RNReactNativeLoginWithAmazon.requestAuthorization();
-    console.log('[Authorization]', JSON.stringify(response, null, 2));
-    return Promise.resolve(response);
-  } catch (e) {
-    console.error('[Authorization] - error', JSON.stringify(e, null, 2));
-    return Promise.reject(e);
-  }
+  return RNReactNativeLoginWithAmazon.requestAuthorization();
 }
 
 export async function getUserInformation() {
-  try {
-    const userInfoStatus = RNReactNativeLoginWithAmazon.userInfoStatus;
-    const response = await RNReactNativeLoginWithAmazon.getUserInformation();
-    console.log('[UserInformation]', JSON.stringify(response, null, 2));
-    return Promise.resolve(response);
-  } catch (e) {
-    console.error('[UserInformation] - error', JSON.stringify(e, null, 2));
-    return Promise.reject(e);
-  }
+  return RNReactNativeLoginWithAmazon.getUserInformation();
 }
 
 export async function signOut() {
-  try {
-    const response = await RNReactNativeLoginWithAmazon.signOut();
-    console.log('[Authorization Signout]', JSON.stringify(response, null, 2));
-    return Promise.resolve(response);
-  } catch (e) {
-    console.error(
-      '[Authorization Signout] - error',
-      JSON.stringify(e, null, 2)
-    );
-    return Promise.reject(e);
-  }
+  return RNReactNativeLoginWithAmazon.signOut();
 }
